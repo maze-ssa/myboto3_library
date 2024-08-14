5c2971a8d6877a9e79ab18129c07a20a9e7a9852
SSA <soesanaung2147@gmail.com>
2024-08-15 00:53:14 +0800

    0115082024

---
+++ listavailablepatch.sh
@@ -0,0 +1,6 @@
+echo #!/bin/bash
+ 
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+echo "InstanceName,PackageName,Date,Month,Year" > $(hostname)$(date +'%Y-%m-%d').csv
+yum list updates | awk -v instance_id=$INSTANCE_ID '{print instance_id "," $1 "," $2 "," $3}' | tail -n +8 >> $(hostname)$(date +'%Y-%m-%d').csv
